c4ac37948c9e7759f5c9e3967e68211a5222e206
WaiMeng Kok <waimeng.kok@ideagenplc.com>
2021-04-14 02:01:26 +0800

    fix build failure

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -18,6 +18,15 @@ pipeline {
                     junit '**/target/surefire-reports/TEST-*.xml'
                     archiveArtifacts 'target/*.jar'
                 }
+                
+                changed {
+                    emailext subject: 'Job \'${JOB_NAME}\' (${BUILD_NUMBER}) is waiting for input', 
+                         attachLog: true, 
+                         body: 'Please go to ${BUILD_URL} and verify the build', 
+                         compressLog: true,
+                         recipientProviders: [upstreamDevelopers(), requestor()], 
+                         to: "test@jenkins"
+                }
             }
         }
     }
